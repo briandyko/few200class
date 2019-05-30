8359eb98ce627dc001c432c370ef2d227bace557
Brian Dyko <briandyko@gmail.com>
2019-05-30 11:08:15 -0400

    added completed functions

--- src/app/components/todo-list/list/list.component.ts
+++ src/app/components/todo-list/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { TodoListItem } from '../models';
 
 @Component({
@@ -9,6 +9,8 @@ import { TodoListItem } from '../models';
 export class ListComponent implements OnInit {
 
   @Input() list: TodoListItem[];
+  @Output() completedCleared = new EventEmitter();
+  @Output() markedComplete = new EventEmitter<TodoListItem>();
   constructor() { }
 
   ngOnInit() {
@@ -16,10 +18,10 @@ export class ListComponent implements OnInit {
 
 
   markComplete(item: TodoListItem) {
-    // TODO: Make this work
+    this.markedComplete.emit(item);
   }
 
   clearCompleted() {
-    // TODO: Make this work
+    this.completedCleared.emit();
   }
 }
